62f4f76cd9fd92b68a3fa45244948c6922bb4e95
Junfeng Ou <junfengou@Macs-MacBook-Pro.local>
2021-11-16 08:12:12 -0600

    Changed rest framework permission for signup to AllowAny

--- product_review/review_stuff/views.py
+++ product_review/review_stuff/views.py
@@ -3,14 +3,14 @@ from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 from .serializers import *
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework import generics
 
 
 class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # permission_classes = (AllowAny, )
+    permission_classes = (AllowAny, )
 
 
 class UserList(generics.ListAPIView):
